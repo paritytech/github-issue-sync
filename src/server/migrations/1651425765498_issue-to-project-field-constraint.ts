fcdd2f1a0f991d58a82950dfa347b90b5eb1a167
joao-paulo-parity <joao-paulo@parity.io>
2022-05-01 14:52:53 -0300

    put optional fields constraints in the database

---
+++ src/server/migrations/1651425765498_issue-to-project-field-constraint.ts
@@ -0,0 +1,28 @@
+import type { MigrationBuilder } from "node-pg-migrate"
+
+import {
+  issueToProjectFieldRuleTable,
+  projectField,
+  projectFieldValue,
+} from "./1645593356645_issue_to_project_field_rule"
+
+const projectFieldBothNullableOrRequiredConstraint =
+  "project_field_both_nullable_or_required"
+
+export const up = async (pgm: MigrationBuilder) => {
+  pgm.addConstraint(
+    issueToProjectFieldRuleTable,
+    projectFieldBothNullableOrRequiredConstraint,
+    `CHECK (
+      (${projectField} is null AND ${projectFieldValue} is null) OR
+      (${projectField} is not null AND ${projectFieldValue} is not null)
+    )`,
+  )
+}
+
+export const down = async (pgm: MigrationBuilder) => {
+  pgm.dropConstraint(
+    issueToProjectFieldRuleTable,
+    projectFieldBothNullableOrRequiredConstraint,
+  )
+}
