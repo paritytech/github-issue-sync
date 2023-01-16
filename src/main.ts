13685673ec497bc7418ce8c99c876222d32659c7
Javier Bullrich <javier@bullrich.dev>
2023-01-16 15:32:50 +0100

    moved logic to synchronizer class

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,44 @@
-import { debug, getInput, info, notice, setFailed, warning } from "@actions/core";
+import { getInput, info, setFailed } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
+import { CoreLogger } from "./github/CoreLogger";
 import { IssueApi } from "./github/issueKit";
 import { ProjectKit } from "./github/projectKit";
-import { Issue } from "./github/types";
-import { Synchronizer } from "./synchronizer";
+import { GitHubContext, Synchronizer } from "./synchronizer";
 
-type ActionInputs = { repoToken: string, orgToken: string, projectNumber: number };
 
-type EventNames = "workflow_dispatch" | "issues";
-
-const main = async (inputs: ActionInputs) => {
-    info("Starting event of type " + context.eventName);
-
-    const { repo, eventName } = context;
-
-    const kit = getOctokit(inputs.repoToken);
-    const issueKit = new IssueApi(kit, repo);
-    const projectGraphQl = getOctokit(inputs.orgToken).graphql.defaults({ headers: { authorization: `token ${inputs.orgToken}` } });
-    const projectKit = new ProjectKit(projectGraphQl, repo, inputs.projectNumber);
-
-    const synchronizer = new Synchronizer(issueKit, projectKit);
-
-    if ((eventName as EventNames) === "workflow_dispatch") {
-        // ! remember to put this in the documentation and log it with some info tag
-        const excludeClosed = (context.payload.inputs.excludeClosed as "true" | "false") === "true";
-        notice(excludeClosed ? "Closed issues will NOT be synced." : "Closed issues will be synced.");
-        await synchronizer.updateAllIssues(excludeClosed);
-    } else if ((eventName as EventNames) === "issues") {
-        const { issue } = context.payload;
-        if (!issue) {
-            throw new Error("Issue payload object was null");
-        }
-        debug(`Received issue ${JSON.stringify(context.payload.issue)}`);
-        info(`Assigning issue #${issue?.number} to project`);
-        await synchronizer.updateOneIssue(issue as Issue);
-    } else {
-        const failMessage = `Event '${eventName}' is not expected. Failing.`;
-        warning(failMessage)
-        setFailed(failMessage);
-        throw new Error(failMessage);
-    }
-
-}
-
-const getInputs = (): ActionInputs => {
+//** Generates the class that will handle the project logic */
+const generateSynchronizer = (): Synchronizer => {
     const repoToken = getInput("GITHUB_TOKEN", { required: true });
     const orgToken = getInput("PROJECT_TOKEN", { required: true });
 
     const projectNumber = parseInt(getInput("project", { required: true }));
     // TODO: Add support for custom project fields (https://docs.github.com/en/issues/planning-and-tracking-with-projects/understanding-fields)
 
-    return { repoToken, orgToken, projectNumber };
+    const { repo } = context;
+
+    const kit = getOctokit(repoToken);
+    const issueKit = new IssueApi(kit, repo);
+    const projectGraphQl = getOctokit(orgToken).graphql.defaults({ headers: { authorization: `token ${orgToken}` } });
+    const logger = new CoreLogger();
+    const projectKit = new ProjectKit(projectGraphQl, repo, projectNumber, logger);
+
+    return new Synchronizer(issueKit, projectKit, logger);
 }
 
-const inputs = getInputs();
+const synchronizer = generateSynchronizer();
 
-main(inputs)
+const { issue } = context.payload;
+const parsedContext: GitHubContext = {
+    eventName: context.eventName,
+    payload: {
+        inputs: context.payload.inputs,
+        issue: issue ? {
+            number: issue.number,
+            node_id: issue.node_id
+        } : undefined
+    }
+}
+
+synchronizer.synchronizeIssue(parsedContext)
     .then(() => info("Finished"))
     .catch(setFailed);
--- src/synchronizer.ts
+++ src/synchronizer.ts
@@ -1,11 +1,43 @@
-import { IIssues, IProjectApi, Issue } from "./github/types"
+import { IIssues, ILogger, IProjectApi, Issue } from "./github/types"
 
 
-export type IssueEvent = "opened" | "deleted" | "closed" | "reopened" | "labeled" | "unlabeled" | "transfered"
+type IssueEvent = "opened" | "deleted" | "closed" | "reopened" | "labeled" | "unlabeled" | "transfered";
+
+type EventNames = "workflow_dispatch" | "issues" | string;
+
+
+export type GitHubContext = {
+    eventName: EventNames, payload: {
+        inputs?: { excludeClosed?: "true" | "false" },
+        issue?: Issue
+    }
+}
 
 export class Synchronizer {
-    constructor(private readonly issueKit: IIssues, private readonly projectKit: IProjectApi) {
+    constructor(
+        private readonly issueKit: IIssues,
+        private readonly projectKit: IProjectApi,
+        private readonly logger: ILogger) {
+    }
 
+    async synchronizeIssue(context: GitHubContext) {
+        if (context.eventName === "workflow_dispatch") {
+            const excludeClosed = context.payload.inputs?.excludeClosed === "true";
+            this.logger.notice(excludeClosed ? "Closed issues will NOT be synced." : "Closed issues will be synced.");
+            await this.updateAllIssues(excludeClosed);
+        } else if (context.eventName === "issues") {
+            const { issue } = context.payload;
+            if (!issue) {
+                throw new Error("Issue payload object was null");
+            }
+            this.logger.debug(`Received issue ${JSON.stringify(context.payload.issue)}`);
+            this.logger.info(`Assigning issue #${issue?.number} to project`);
+            await this.updateOneIssue(issue);
+        } else {
+            const failMessage = `Event '${context.eventName}' is not expected. Failing.`;
+            this.logger.warning(failMessage)
+            throw new Error(failMessage);
+        }
     }
 
     async updateAllIssues(excludeClosed: boolean = false) {
