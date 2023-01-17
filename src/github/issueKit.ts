3c78d2b6f75fb9433cfc272182eafcbefb0b9a1c
Javyer <javier.bullrich@parity.io>
2023-01-17 17:38:41 +0100

    added linting to the action (#49)
    Added eslint and prettier and added the step of running the linter on pull requests
    
    Closes #47

--- src/github/issueKit.ts
+++ src/github/issueKit.ts
@@ -1,22 +1,22 @@
 import { GitHub } from "@actions/github/lib/utils";
-import { Octokit } from "@octokit/rest";
+
 import { IIssues, Issue, Repository } from "./types";
 
-
-
 export class IssueApi implements IIssues {
-    /** Requires permissions to the repository with access to the repo */
-    constructor(private readonly octokit: InstanceType<typeof GitHub>, private readonly repoData: Repository) {
-    }
+  /** Requires permissions to the repository with access to the repo */
+  constructor(private readonly octokit: InstanceType<typeof GitHub>, private readonly repoData: Repository) {}
 
-    async getIssueState(issueId: number): Promise<"open" | "closed"> {
-        const { owner, repo } = this.repoData;
-        const issueData = await this.octokit.rest.issues.get({ repo, owner, issue_number: issueId });
-        return issueData.data.state === "open" ? "open" : "closed";
-    }
+  async getIssueState(issueId: number): Promise<"open" | "closed"> {
+    const { owner, repo } = this.repoData;
+    const issueData = await this.octokit.rest.issues.get({ repo, owner, issue_number: issueId });
+    return issueData.data.state === "open" ? "open" : "closed";
+  }
 
-    async getAllIssuesId(excludeClosed: boolean): Promise<Issue[]> {
-        const allIssues = await this.octokit.rest.issues.listForRepo({ ...this.repoData, state: excludeClosed ? "open" : "all" });
-        return allIssues.data;
-    }
+  async getAllIssuesId(excludeClosed: boolean): Promise<Issue[]> {
+    const allIssues = await this.octokit.rest.issues.listForRepo({
+      ...this.repoData,
+      state: excludeClosed ? "open" : "all",
+    });
+    return allIssues.data;
+  }
 }
