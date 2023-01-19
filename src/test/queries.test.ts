71dea7741ed1414a1638b06a33800c890ab5c9a7
Javyer <javier.bullrich@parity.io>
2023-01-19 15:27:31 +0100

    fix: updated GraphQL queries to V2 (#50)
    Updated GraphQL queries to support version 2 as `ProjectNext` has been deprecated.
    
    Moved all the queries to be stored in a exported variables so they can be accessed from outside.
    
    Created unit tests which validates the schemas using `@octokit/graphql-schema` which contains the latest schemas. 
    
    Modified dependabot to keep the `@octokit/graphql-schema` dependency at its latest.
    
    This commit closes #46.

---
+++ src/test/queries.test.ts
@@ -0,0 +1,21 @@
+import { validate } from "@octokit/graphql-schema";
+
+import {
+  ADD_PROJECT_V2_ITEM_BY_ID_QUERY,
+  PROJECT_V2_QUERY,
+  UPDATE_PROJECT_V2_ITEM_FIELD_VALUE_QUERY,
+} from "src/github/projectKit";
+
+describe("Schemas", () => {
+  test("PROJECT_V2_QUERY", () => {
+    expect(validate(PROJECT_V2_QUERY)).toEqual([]);
+  });
+
+  test("ADD_PROJECT_V2_ITEM_BY_ID_QUERY", () => {
+    expect(validate(ADD_PROJECT_V2_ITEM_BY_ID_QUERY)).toEqual([]);
+  });
+
+  test("UPDATE_PROJECT_V2_ITEM_FIELD_VALUE_QUERY", () => {
+    expect(validate(UPDATE_PROJECT_V2_ITEM_FIELD_VALUE_QUERY)).toEqual([]);
+  });
+});
