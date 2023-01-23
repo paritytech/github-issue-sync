0dd8aebce801d81ce0a4b58f3095095f2abdadcf
Javyer <javier.bullrich@parity.io>
2023-01-23 18:23:28 +0100

    feat: added custom field support assigment (#51)
    Added the ability to add a custom project field when assigning an issue to a project.
    
    This commit closes #44 
    
    Moved more of the logic to the `synchronizer` and enhanced the tests to include those business logic changes.
    
    Created an evaluated new GraphQL query for GitHub's project API.
    
    Co-authored-by: Przemek Rzad <roopert7@gmail.com>

--- src/github/projectKit.ts
+++ src/github/projectKit.ts
@@ -1,8 +1,9 @@
 import { graphql } from "@octokit/graphql";
 
-import { ILogger, IProjectApi, Issue, Repository } from "./types";
+import { FieldValues, ILogger, IProjectApi, Issue, Repository } from "./types";
 
 type NodeData = { id: string; title: string };
+type FieldData = { name: string; id: string; options?: { name: string; id: string }[] };
 
 export const PROJECT_V2_QUERY: string = `
 query($organization: String!, $number: Int!) {
@@ -20,6 +21,41 @@ mutation($project: ID!, $issue: ID!) {
   addProjectV2ItemById(input: {projectId: $project, contentId: $issue}) {
     item {
       id
+    }
+  }
+}
+`;
+
+export const PROJECT_FIELD_ID_QUERY: string = `
+query($project: ID!) {
+  node(id: $project) {
+    ... on ProjectV2 {
+      fields(first: 20) { 
+        nodes {
+          ... on ProjectV2Field {
+            id
+            name
+          }
+          ... on ProjectV2IterationField {
+            id
+            name
+            configuration {
+              iterations {
+                startDate
+                id
+              }
+            }
+          }
+          ... on ProjectV2SingleSelectField {
+            id
+            name
+            options {
+              id
+              name
+            }
+          }
+        }
+      }
     }
   }
 }
@@ -65,20 +101,38 @@ export class ProjectKit implements IProjectApi {
     private readonly logger: ILogger,
   ) {}
 
-  /* 
-  changeIssueStateInProject(issueCardId: number, state: "todo" | "in progress" | "blocked" | "done"): Promise<void> {
-    return this.gql(UPDATE_STATE_IN_PROJECT_QUERY, {
-      project: this.projectNumber,
-      item: issueCardId,
-      targetField: "Status",
-      targetFieldValue: state,
-    });
-  } */
+  async changeIssueStateInProject(issueCardId: string, project: NodeData, fields: FieldValues): Promise<void> {
+    try {
+      const op = await this.gql(UPDATE_PROJECT_V2_ITEM_FIELD_VALUE_QUERY, {
+        project: project.id,
+        item: issueCardId,
+        targetField: fields.field,
+        targetFieldValue: fields.value,
+      });
+
+      this.logger.debug("Returned " + JSON.stringify(op));
+    } catch (e) {
+      throw new Error("Failed while executing the 'UPDATE_PROJECT_V2_ITEM_FIELD_VALUE_QUERY' query", { cause: e });
+    }
+  }
 
   /**
-   * Fetches the node id from the project id and caches it.
-   * @returns node_id of the project. This value never changes so caching it per instance is effective
+   * Get all the project fields in a project, with their node ids and available options
+   * @returns A collection of all the fields available in the issue item
    */
+  async getProjectFields(projectId: string): Promise<FieldData[]> {
+    try {
+      type returnType = { node: { fields: { nodes: FieldData[] } } };
+      const projectData = await this.gql<returnType>(PROJECT_FIELD_ID_QUERY, { project: projectId });
+
+      this.logger.debug("correct node data: " + JSON.stringify(projectData.node.fields.nodes[0]));
+      return projectData.node.fields.nodes;
+    } catch (e) {
+      this.logger.error("Failed while executing the 'PROJECT_V2_QUERY' query");
+      throw e;
+    }
+  }
+
   async fetchProjectData(): Promise<NodeData> {
     if (this.projectNode) {
       return this.projectNode;
@@ -95,8 +149,7 @@ export class ProjectKit implements IProjectApi {
 
       return projectData.organization.projectV2;
     } catch (e) {
-      this.logger.error("Failed while executing the 'PROJECT_V2_QUERY' query");
-      throw e;
+      throw new Error("Failed while executing the 'PROJECT_V2_QUERY' query", { cause: e });
     }
   }
 
@@ -109,39 +162,55 @@ export class ProjectKit implements IProjectApi {
     await this.gql(UPDATE_PROJECT_V2_ITEM_FIELD_VALUE_QUERY, { project, item, targetField, targetFieldValue });
   }
 
-  async assignIssueToProject(issue: Issue, projectId: string): Promise<boolean> {
+  async assignIssueToProject(issue: Issue, projectId: string): Promise<string> {
     try {
       const migration = await this.gql<{ addProjectV2ItemById: { item: { id: string } } }>(
         ADD_PROJECT_V2_ITEM_BY_ID_QUERY,
         { project: projectId, issue: issue.node_id },
       );
 
-      return !!migration.addProjectV2ItemById.item.id;
+      return migration.addProjectV2ItemById.item.id;
     } catch (e) {
-      this.logger.error("Failed while executing 'ADD_PROJECT_V2_ITEM_BY_ID_QUERY' query");
-      throw e;
+      throw new Error("Failed while executing 'ADD_PROJECT_V2_ITEM_BY_ID_QUERY' query", { cause: e });
     }
   }
 
-  async addIssueToProject(issue: Issue, project: NodeData): Promise<boolean> {
+  async fetchProjectFieldNodeValues(project: NodeData, projectFields?: FieldValues): Promise<FieldValues> {
+    if (!projectFields) {
+      throw new Error("'projectsFields' is null!");
+    }
+
+    const projectFieldData = await this.getProjectFields(project.id);
+
+    const { field, value } = projectFields;
+
+    // ? Should we use .localeCompare here?
+    const customField = projectFieldData.find(({ name }) => name.toUpperCase() === field.toUpperCase());
+
+    // check that this custom field exists and it has available options to set up
+    if (!customField) {
+      throw new Error(`Field ${field} does not exist!`);
+    } else if (!customField.options) {
+      throw new Error(`Field ${field} does not have any available options!.` + "Please add options to set values");
+    }
+
+    this.logger.debug(`Custom field '${field}' was found.`);
+
+    // search for the node element with the correct name.
+    const fieldOption = customField.options.find(({ name }) => name.toUpperCase() === value.toUpperCase());
+    if (!fieldOption) {
+      const valuesArray = customField.options.map((options) => options.name);
+      throw new Error(`Project value '${value}' does not exist. Available values are ${JSON.stringify(valuesArray)}`);
+    }
+
+    this.logger.debug(`Field options '${value}' was found.`);
+
+    return { field: customField.id, value: fieldOption.id };
+  }
+
+  async assignIssue(issue: Issue, project: NodeData): Promise<string> {
     this.logger.info(`Syncing issue #${issue.number} for ${project.title}`);
 
     return await this.assignIssueToProject(issue, project.id);
-  }
-
-  async assignIssue(issue: Issue): Promise<boolean> {
-    const project = await this.fetchProjectData();
-
-    return await this.addIssueToProject(issue, project);
-
-    // TODO: Assign targetField
-  }
-
-  async assignIssues(issues: Issue[]): Promise<boolean[]> {
-    const project = await this.fetchProjectData();
-
-    const issueAssigment = issues.map((issue) => this.addIssueToProject(issue, project));
-
-    return await Promise.all(issueAssigment);
   }
 }
--- src/test/synchronizer.test.ts
+++ src/test/synchronizer.test.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 import { mock, mockReset } from "jest-mock-extended";
 
-import { IIssues, ILogger, IProjectApi, Issue } from "src/github/types";
-import { Synchronizer } from "src/synchronizer";
+import { IIssues, ILogger, IProjectApi, Issue, NodeData } from "src/github/types";
+import { GitHubContext, Synchronizer } from "src/synchronizer";
 
 describe("Synchronizer tests", () => {
   const issueKit = mock<IIssues>();
@@ -17,57 +17,154 @@ describe("Synchronizer tests", () => {
     synchronizer = new Synchronizer(issueKit, projectKit, logger);
   });
 
-  test("should fail on invalid event name", async () => {
-    const randomEventName = new Date().toDateString();
-    const expectedError = `Event '${randomEventName}' is not expected. Failing.`;
-    await expect(synchronizer.synchronizeIssue({ eventName: randomEventName, payload: {} })).rejects.toThrow(
-      expectedError,
-    );
-  });
-
-  test("should fail on issue event without payload", async () => {
-    await expect(synchronizer.synchronizeIssue({ eventName: "issues", payload: {} })).rejects.toThrow(
-      "Issue payload object was null",
-    );
-  });
-
-  test("should log when all issues will be synced", async () => {
-    issueKit.getAllIssues.mockReturnValue(Promise.resolve([]));
-    await synchronizer.synchronizeIssue({ eventName: "workflow_dispatch", payload: {} });
-
-    expect(logger.notice).toBeCalledWith("Closed issues will be synced.");
-  });
-
-  test("should log when only open issues will be synced", async () => {
-    issueKit.getAllIssues.mockReturnValue(Promise.resolve([]));
-    await synchronizer.synchronizeIssue({
-      eventName: "workflow_dispatch",
-      payload: { inputs: { excludeClosed: "true" } },
+  describe("synchronize Issues function", () => {
+    test("should fail on invalid event name", async () => {
+      const randomEventName = new Date().toDateString();
+      const expectedError = `Event '${randomEventName}' is not expected. Failing.`;
+      await expect(synchronizer.synchronizeIssue({ eventName: randomEventName, payload: {} })).rejects.toThrow(
+        expectedError,
+      );
     });
 
-    expect(logger.notice).toBeCalledWith("Closed issues will NOT be synced.");
-  });
-
-  test("should invoke project.assignIssue", async () => {
-    const issueNumber = Math.floor(Math.random() * 100);
-    await synchronizer.synchronizeIssue({
-      eventName: "issues",
-      payload: { issue: { node_id: "1234321", number: issueNumber } },
+    test("should fail on issue event without payload", async () => {
+      await expect(synchronizer.synchronizeIssue({ eventName: "issues", payload: {} })).rejects.toThrow(
+        "Issue payload object was null",
+      );
     });
 
-    logger.info.calledWith(`Assigning issue #${issueNumber} to project`);
-    projectKit.assignIssue.calledWith({ node_id: "1234321", number: issueNumber });
+    test("should log when all issues will be synced", async () => {
+      issueKit.getAllIssues.mockResolvedValue([]);
+      await synchronizer.synchronizeIssue({ eventName: "workflow_dispatch", payload: {} });
+
+      expect(logger.notice).toBeCalledWith("Closed issues will be synced.");
+    });
+
+    test("should log when only open issues will be synced", async () => {
+      issueKit.getAllIssues.mockResolvedValue([]);
+      await synchronizer.synchronizeIssue({
+        eventName: "workflow_dispatch",
+        payload: { inputs: { excludeClosed: "true" } },
+      });
+
+      expect(logger.notice).toBeCalledWith("Closed issues will NOT be synced.");
+    });
   });
 
-  test("should call project.assignIssues with an iteration", async () => {
-    const issues: Issue[] = [
-      { number: 123, node_id: "asd_dsa" },
-      { number: 987, node_id: "poi_lkj" },
-    ];
-    issueKit.getAllIssues.mockReturnValue(Promise.resolve(issues));
-    projectKit.assignIssues.mockReturnValue(Promise.resolve([true, true]));
-    await synchronizer.synchronizeIssue({ eventName: "workflow_dispatch", payload: {} });
+  describe("update one issue", () => {
+    let nodeData: NodeData;
+    let ctx: GitHubContext;
+    let issueNumber: number;
+    beforeEach(() => {
+      issueNumber = 123;
+      nodeData = { id: new Date().toDateString(), title: "Update one issue" };
+      ctx = { eventName: "issues", payload: { issue: { node_id: "update_one_issue", number: issueNumber } } };
+      projectKit.fetchProjectData.mockResolvedValue(nodeData);
+    });
 
-    projectKit.assignIssues.calledWith(issues);
+    test("should invoke using the correct node data", async () => {
+      await synchronizer.synchronizeIssue(ctx);
+      expect(logger.info).toBeCalledWith(`Assigning issue #${issueNumber} to project`);
+      expect(projectKit.assignIssue).toBeCalledWith(ctx.payload.issue, nodeData);
+    });
+
+    test("should not throw on correct execution", async () => {
+      await synchronizer.synchronizeIssue(ctx);
+      expect(logger.info).toBeCalledWith(`Assigning issue #${issueNumber} to project`);
+    });
+
+    test("should fetch custom fields data", async () => {
+      const [field, value] = ["a", "B"];
+      await synchronizer.synchronizeIssue({ ...ctx, config: { projectField: { field, value } } });
+      expect(projectKit.fetchProjectFieldNodeValues).toBeCalledWith(nodeData, { field, value });
+    });
+
+    test("should assign custom fields", async () => {
+      const nodeValueData = { field: "eee", value: "fff" };
+      projectKit.fetchProjectFieldNodeValues.mockResolvedValue(nodeValueData);
+      const issueCardNodeId = "issue_node_id_example";
+      projectKit.assignIssue.mockResolvedValue(issueCardNodeId);
+      await synchronizer.synchronizeIssue({ ...ctx, config: { projectField: { field: "c", value: "d" } } });
+      expect(projectKit.changeIssueStateInProject).toBeCalledWith(issueCardNodeId, nodeData, nodeValueData);
+    });
+
+    test("should throw error while assigning invalid custom field", async () => {
+      projectKit.fetchProjectFieldNodeValues.mockRejectedValue(new Error());
+      await expect(
+        synchronizer.synchronizeIssue({ ...ctx, config: { projectField: { field: "c", value: "d" } } }),
+      ).rejects.toThrow("Failed fetching project values");
+      expect(projectKit.changeIssueStateInProject).toHaveBeenCalledTimes(0);
+    });
+  });
+  describe("update all issues", () => {
+    let nodeData: NodeData;
+    let ctx: GitHubContext;
+    beforeEach(() => {
+      nodeData = { id: new Date().toDateString(), title: "Update all issues" };
+      ctx = { eventName: "workflow_dispatch", payload: {} };
+      projectKit.fetchProjectData.mockResolvedValue(nodeData);
+    });
+
+    test("should report when no issues are available", async () => {
+      issueKit.getAllIssues.mockResolvedValue([]);
+      expect(await synchronizer.synchronizeIssue(ctx)).toBeFalsy();
+      expect(logger.notice).toBeCalledWith("No issues found");
+    });
+
+    test("should call assign Issues with an iteration", async () => {
+      const issues: Issue[] = [
+        { number: 1, node_id: "asd_dsa" },
+        { number: 2, node_id: "poi_lkj" },
+        { number: 3, node_id: "grs_Dfr" },
+        { number: 4, node_id: "hor_2dg" },
+      ];
+
+      issueKit.getAllIssues.mockResolvedValue(issues);
+      await synchronizer.synchronizeIssue(ctx);
+
+      for (let i = 0; i < issues.length; i++) {
+        expect(projectKit.assignIssue).toHaveBeenNthCalledWith(i + 1, issues[i], nodeData);
+      }
+      expect(logger.debug).toBeCalledWith(`Finished assigning ${issues.length} issues`);
+    });
+
+    test("should fetch custom fields data", async () => {
+      const [field, value] = ["a", "B"];
+      issueKit.getAllIssues.mockResolvedValue([{ number: 999, node_id: "123_asd" }]);
+      await synchronizer.synchronizeIssue({ ...ctx, config: { projectField: { field, value } } });
+      expect(projectKit.fetchProjectFieldNodeValues).toBeCalledWith(nodeData, { field, value });
+    });
+
+    test("should assign custom fields", async () => {
+      const issues: Issue[] = [
+        { number: 1, node_id: "asdw_dsa" },
+        { number: 2, node_id: "gers_Dfr" },
+        { number: 4, node_id: "hgor_2dg" },
+        { number: 8, node_id: "pdoi_lkj" },
+      ];
+      const nodeValueData = { field: "eee", value: "fff" };
+      issueKit.getAllIssues.mockResolvedValue(issues);
+      projectKit.fetchProjectFieldNodeValues.mockResolvedValue(nodeValueData);
+
+      let index = 1;
+      projectKit.assignIssue
+        .mockResolvedValueOnce(`_${index++}`)
+        .mockResolvedValueOnce(`_${index++}`)
+        .mockResolvedValueOnce(`_${index++}`)
+        .mockResolvedValueOnce(`_${index++}`);
+
+      await synchronizer.synchronizeIssue({ ...ctx, config: { projectField: { field: "ooo", value: "iiii" } } });
+      for (let i = 1; i < issues.length; i++) {
+        expect(projectKit.changeIssueStateInProject).toHaveBeenNthCalledWith(i, `_${i}`, nodeData, nodeValueData);
+      }
+    });
+
+    test("should throw error while assigning invalid custom field", async () => {
+      issueKit.getAllIssues.mockResolvedValue([{ number: 999, node_id: "123_asd" }]);
+      projectKit.fetchProjectFieldNodeValues.mockRejectedValue(new Error());
+      await expect(
+        synchronizer.synchronizeIssue({ ...ctx, config: { projectField: { field: "k", value: "y" } } }),
+      ).rejects.toThrow("Failed fetching project values");
+      expect(projectKit.changeIssueStateInProject).toHaveBeenCalledTimes(0);
+    });
   });
 });
