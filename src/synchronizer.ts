2bf8df08e3d0750367f80fa8b81187c3fd8a3868
Javier Bullrich <javier@bullrich.dev>
2023-01-24 17:19:26 +0100

    added method to decide if issue should be assigned

--- src/synchronizer.ts
+++ src/synchronizer.ts
@@ -24,6 +24,8 @@ export type GitHubContext = {
   };
 };
 
+const toLowerCase = (array: string[]): string[] => array.map((a) => a.toLowerCase());
+
 export class Synchronizer {
   constructor(
     private readonly issueKit: IIssues,
@@ -49,6 +51,87 @@ export class Synchronizer {
       this.logger.warning(failMessage);
       throw new Error(failMessage);
     }
+  }
+
+  /**
+   * Labels can be either an array of objects or an array of string (or maybe both?)
+   * This functions cleans them and returns all the labels names as a string array
+   */
+  private convertLabelArray(labels?: (string | { name?: string })[]): string[] {
+    if (!labels || labels.length === 0) {
+      return [];
+    }
+    const list: string[] = [];
+
+    labels.forEach((label) => {
+      if (typeof label === "string" || label instanceof String) {
+        list.push(label as string);
+      } else if (label.name) {
+        list.push(label.name);
+      }
+    });
+
+    return list;
+  }
+
+  /**
+   * Method which takes all of the (predicted) cases and calculates if the issue should be assigned of skipped
+   * @param payload object which contains both the event, the issue type and it's information
+   * @param labels labels required for the action. Can be null or empty
+   * @returns true if the label should be assigned, false if it should be skipped
+   */
+  private shouldAssignIssue(payload: Payload, labels?: string[]): boolean {
+    const action = payload.action as IssueEvent;
+
+    if (action === "labeled") {
+      // If this is a labeling event but there are no labels in the config we skip them
+      if (!labels || labels.length === 0) {
+        this.logger.notice("No required labels found. Skipping assignment.");
+        return false;
+      }
+
+      const labelName = payload.label?.name;
+      // Shouldn't happen. Throw and find out what is this kind of event.
+      if (!labelName) {
+        throw new Error("No label found in a labeling event!");
+      }
+
+      if (toLowerCase(labels).indexOf(labelName.toLowerCase()) > -1) {
+        this.logger.info(`Found matching label '${labelName}'`);
+        return true;
+      } else {
+        this.logger.notice(`Label '${labelName}' does not match any of the labels '${labels}'. Skipping.`);
+        return false;
+      }
+    } else if (action === "unlabeled") {
+      this.logger.warning("No support for 'unlabeled' event yet. Skipping");
+      return false;
+    } else {
+      // if no labels are required and this is not a labeling event, assign the issue.
+      if (!labels || labels.length === 0) {
+        this.logger.info("Matching requirements: not a labeling event and no labels found in the configuration.");
+        return true;
+      }
+      // if the issue in this event has labels and a matching label config, assign it.
+      const issueLabels = payload.issue?.labels ?? null;
+      if (labels.length > 0 && issueLabels && issueLabels.length > 0) {
+        // complex query. Sanitizing everything to a lower case string array first
+        const parsedLabels = toLowerCase(this.convertLabelArray(issueLabels));
+        const requiredLabels = toLowerCase(labels);
+        // checking if an element in one array is included in the second one
+        const matchingElement = parsedLabels.some((pl) => requiredLabels.includes(pl));
+        if (matchingElement) {
+          this.logger.info(
+            `Found matching element between ${JSON.stringify(parsedLabels)} and ${JSON.stringify(labels)}`,
+          );
+          return true;
+        }
+        return false;
+      }
+    }
+
+    this.logger.debug(`Case ${action} not considered. Accepted with the following payload: `, payload);
+    return true;
   }
 
   /**
