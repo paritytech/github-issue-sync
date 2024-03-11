2dea89f8234a79edb4d2aaca25c1709bc6054a78
Javier Bullrich <javier@bullrich.dev>
2024-03-11 15:02:21 +0100

    updated .eslint files to match eng-automation/js-styles

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,16 @@
-const { getConfiguration } = require("opstooling-js-style/src/eslint/configuration");
-
-const tsConfParams = { rootDir: __dirname };
-
-const conf = getConfiguration({ typescript: tsConfParams });
-
-module.exports = conf;
+const {
+    getConfiguration,
+    getTypescriptOverride,
+  } = require("@eng-automation/js-style/src/eslint/configuration");
+  
+  const tsConfParams = { rootDir: __dirname };
+  
+  const conf = getConfiguration({ typescript: tsConfParams });
+  
+  const tsConfOverride = getTypescriptOverride(tsConfParams);
+  conf.overrides.push({
+    ...tsConfOverride,
+    rules: { "@typescript-eslint/strict-boolean-expressions": 0 },
+  });
+  
+  module.exports = conf;
